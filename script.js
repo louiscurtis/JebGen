7263c27ce02d434b5cfc218dfb0fc5b2ee3c12c7
Louis Curtis <louis.curtis@unb.ca>
2021-12-18 16:42:09 -0400

    index basic

---
+++ script.js
@@ -0,0 +1,79 @@
+const expletiveStarts = [
+  "crimple",
+  "cropple",
+  "dodgy",
+  "fiddle",
+  "gobble",
+  "goose",
+  "golly",
+  "rustled",
+];
+
+const expletiveEnds = [
+  "berries",
+  "blighter",
+  "bounder",
+  "jimmies",
+  "knocker",
+  "sticks",
+  "pots",
+  "puff",
+  "wobblers",
+];
+
+const expletiveStandalones = [
+  "Arse",
+  "Beaver",
+  "Bellend",
+  "Berk",
+  "Bint",
+  "Blighter",
+  "Bloodclaat",
+  "Bonk",
+  "Bugger",
+  "Chunder",
+  "Clunge",
+  "Cow",
+  "Fanny",
+  "Feck",
+  "Flaps",
+  "Gash",
+  "Git",
+  "Knob",
+  "knobend",
+  "Hocus pocus",
+  "Minge",
+  "Minger",
+  "Munter",
+  "Nonce",
+  "Prick",
+  "Prickteaser",
+  "Punani",
+  "Shag",
+  "Slag",
+  "Slapper",
+  "Tart",
+  "Wanker",
+];
+
+function setTextContent() {
+  const contentDiv = document.getElementById("gen-content");
+
+  const randomPhraseType = Math.floor(Math.random() * 2);
+
+  if (randomPhraseType === 0) {
+    const randFirst = Math.floor(Math.random() * expletiveStarts.length);
+
+    const randSecond = Math.floor(Math.random() * expletiveEnds.length);
+
+    contentDiv.innerHTML =
+      expletiveStarts[randFirst] + " " + expletiveEnds[randSecond];
+  } else if (randomPhraseType === 1) {
+    const randFirst = Math.floor(Math.random() * expletiveStarts.length);
+
+    const randSecond = Math.floor(Math.random() * expletiveStandalones.length);
+
+    contentDiv.innerHTML =
+      expletiveStarts[randFirst] + " " + expletiveStandalones[randSecond];
+  }
+}
