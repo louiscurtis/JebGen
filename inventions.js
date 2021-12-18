1960ce3725de5aaf3531384bb6812c95cdd86333
Louis Curtis <louis.curtis@unb.ca>
2021-12-18 17:22:32 -0400

    inventions

---
+++ inventions.js
@@ -0,0 +1,50 @@
+const starts = [
+  "Versatile",
+  "Improved",
+  "Superior",
+  "Tricornial",
+  "Wangsational",
+  "Ogdenized",
+  "Vaporized",
+  "Automated",
+  "Auxiliary",
+];
+
+const mids = ["Spinning", "Rump-fed", "Jolly", "Electric", "Pet"];
+
+const ends = [
+  "Poisonator",
+  "Acidifier",
+  "Spontaneator",
+  "Jimmy-Rustler",
+  "Door-Knocker",
+  "Stick",
+  "Pots",
+  "Puffsender",
+  "Wobbler",
+  "Mercurializer",
+];
+
+function setInventionContent() {
+  const insultDiv = document.getElementById("invention");
+
+  const hasMid = Math.floor(Math.random() * 2);
+
+  const randFirst = Math.floor(Math.random() * starts.length);
+
+  const randMid = Math.floor(Math.random() * mids.length);
+
+  const randSecond = Math.floor(Math.random() * ends.length);
+
+  let newPhrase = starts[randFirst];
+
+  if (hasMid === 0) {
+    newPhrase = newPhrase + " " + mids[randMid];
+  }
+
+  const phraseEnd = ends[randSecond];
+
+  newPhrase = newPhrase + " " + phraseEnd;
+
+  insultDiv.innerHTML = newPhrase;
+}
