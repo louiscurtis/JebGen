63af92e5add17fc10dbf6328227c1e572aad3e6c
Louis Curtis <louis.curtis@unb.ca>
2021-12-18 18:41:15 -0400

    french names

--- people.js
+++ people.js
@@ -5,21 +5,58 @@ function setPersonContent() {
 
   const ends = ["Moody", "Jacobson", "Marvin"];
 
-  const hasMid = Math.floor(Math.random() * 2);
-  const randFirst = Math.floor(Math.random() * starts.length);
-  const randMid = Math.floor(Math.random() * mids.length);
-  const randSecond = Math.floor(Math.random() * ends.length);
+  // frenchy
 
-  let newPhrase = starts[randFirst];
+  const startsFr = [
+    "Jean",
+    "Guillaume",
+    "Jeremie",
+    "Galdomet",
+    "Francin",
+    "Jean",
+  ];
 
-  if (hasMid === 0) {
-    newPhrase = newPhrase + " " + mids[randMid];
+  const midsFr = ["Jean"];
+
+  const endsFr = ["Jean", "Baptiste"];
+
+  const isFrench = document.getElementById("french-person").checked;
+
+  if (isFrench) {
+    const hasMid = Math.floor(Math.random() * 2);
+    const randFirst = Math.floor(Math.random() * startsFr.length);
+    const randMid = Math.floor(Math.random() * midsFr.length);
+    const randSecond = Math.floor(Math.random() * endsFr.length);
+
+    let newPhrase = startsFr[randFirst];
+
+    if (hasMid === 0) {
+      newPhrase = newPhrase + " " + midsFr[randMid];
+    }
+
+    const phraseEnd = endsFr[randSecond];
+
+    newPhrase = newPhrase + " " + phraseEnd;
+
+    const personDiv = document.getElementById("person");
+    personDiv.innerHTML = newPhrase;
+  } else {
+    const hasMid = Math.floor(Math.random() * 2);
+    const randFirst = Math.floor(Math.random() * starts.length);
+    const randMid = Math.floor(Math.random() * mids.length);
+    const randSecond = Math.floor(Math.random() * ends.length);
+
+    let newPhrase = starts[randFirst];
+
+    if (hasMid === 0) {
+      newPhrase = newPhrase + " " + mids[randMid];
+    }
+
+    const phraseEnd = ends[randSecond];
+
+    newPhrase = newPhrase + " " + phraseEnd;
+
+    const personDiv = document.getElementById("person");
+    personDiv.innerHTML = newPhrase;
   }
-
-  const phraseEnd = ends[randSecond];
-
-  newPhrase = newPhrase + " " + phraseEnd;
-
-  const personDiv = document.getElementById("person");
-  personDiv.innerHTML = newPhrase;
 }
