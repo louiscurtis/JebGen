28c64b7b1e01d6e30a5c57089fa2745ad9b6a8ec
Louis Curtis <louis.curtis@unb.ca>
2021-12-27 17:09:58 -0400

    angry

--- toCasing.js
+++ toCasing.js
@@ -1,8 +1,12 @@
 function toCasing(inputString) {
   const shouldLower = document.getElementById("casing-toggle").checked;
 
+  const shouldAngry = document.getElementById("angry-toggle").checked;
+
   if (shouldLower) {
     return inputString[0].toLowerCase() + inputString.substring(1);
+  } else if (shouldAngry) {
+    return inputString.toUpperCase() + "!";
   } else {
     return inputString[0].toUpperCase() + inputString.substring(1);
   }
